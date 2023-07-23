28c6f9fb0bc8c7af49fbc49e81fdb7953f216ce6
branmeco <branmeco17@gmail.com>
2023-07-23 14:26:37 -0500

    MongoDB Setup

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -21,3 +22,8 @@ server.listen(8080, () => {
     console.log("Server running on http://localhost:8080/");
 });
 
+const MONGO_URL = "mongodb+srv://root:Aa8ge0050@cluster0.uk5rgex.mongodb.net/Login";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => console.log(error));
