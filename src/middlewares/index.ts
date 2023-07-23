37bdff81e2d1169815491b19316da3089b7c6ca5
branmeco <branmeco17@gmail.com>
2023-07-23 18:59:37 -0500

    Middelware creation

---
+++ src/middlewares/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+
+import { get, merge } from 'lodash';
+
+import { getUserBySessionToken } from "models/users";
+
+export const isAuthentication = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const sessionToken = req.cookies('NEO-AUTH');
+
+        if (!sessionToken) {
+            return res.sendStatus(403);
+        }
+
+        const existingUser = await getUserBySessionToken(sessionToken);
+
+        if (!existingUser) {
+            return res.sendStatus(403);
+        }
+
+        merge(req, { identity: existingUser });
+
+        return next();
+
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
